ab35d5258db6551b7f1b24eee53083aa98d65abd
Honey Modi <Honey>
2021-06-06 15:10:59 -0400

    Creating security group

--- main.tf
+++ main.tf
@@ -1,17 +1,17 @@
 resource "aws_security_group" "SG-Honey" {
-  ingress = [ {
+  ingress = {
     cidr_blocks = [ "99.227.118.13" ]
     from_port = 8080
     to_port = 8080
     protocol = "tcp"
-  } ]
+  } 
 
-  egress = [ {
+  egress =  {
     cidr_blocks = [ "0.0.0.0/0" ]
     from_port = 0
     to_port = 0
     protocol = "-1"
-  } ]
+  } 
 }
 
 provider "aws" {
